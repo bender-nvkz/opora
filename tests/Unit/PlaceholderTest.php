975fc82d9b8558259fe0ebc08619dabe56365f2c
Artem Iksanov <iksanov@meiji.media>
2026-05-20 06:47:03 +0500

    test(core): add placeholder test for initial setup

---
+++ tests/Unit/PlaceholderTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Opora\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+final class PlaceholderTest extends TestCase
+{
+    public function testPlaceholder(): void
+    {
+        $this->assertTrue(true);
+    }
+}
